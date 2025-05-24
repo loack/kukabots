c40b61dfe55a7d44fb99760df4cee27385a16692
loic <loic.tournier@gmail.com>
2025-05-24 16:21:14 +0200

    modifying sps loop

--- kukavar_client/change_variable.py
+++ kukavar_client/change_variable.py
@@ -2,8 +2,8 @@ from kukavarproxy import KukaVarProxyClient
 
 robot = KukaVarProxyClient('192.168.1.5',7000)
 robot.connect()
-variable_name = "P_SELECT"
-new_value = 4
+variable_name = "TRIGGER_START"
+new_value = "TRUE"
 
 print(robot.read(variable_name))
 robot.write(variable_name,new_value)
