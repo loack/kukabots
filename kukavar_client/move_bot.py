1726412ef9d19914889a75f2ade4266f5e78ebea
loic <loic.tournier@gmail.com>
2025-06-01 18:40:35 +0200

    lancement du robot à distance avec KVP_START

--- kukavar_client/move_bot.py
+++ kukavar_client/move_bot.py
@@ -2,9 +2,9 @@ from kukavarproxy import KukaVarProxyClient
 
 robot = KukaVarProxyClient('192.168.1.5',7000)
 robot.connect()
+
 print(robot.read("P1"))
 current_position = robot.read("$AXIS_ACT")
 print("Current Robot Position: ", current_position)
-target_position = "{E6AXIS: A1 0.0, A2 -90.00000, A3 89.99999, A4 0.0, A5 0.0, A6 60.0, E1 0.0, E2 0.0, E3 0.0, E4 0.0, E5 0.0, E6 0.0}"
-print(robot.write("P1", target_position))
+print(robot.write("P1", current_position))
 robot.disconnect()
--- kukavar_client/read_variable copy.py
+++ kukavar_client/read_variable copy.py
@@ -2,7 +2,7 @@ from kukavarproxy import KukaVarProxyClient
 
 robot = KukaVarProxyClient('192.168.1.5',7000)
 robot.connect()
-variable_name = "USER"
+variable_name = "HOME"
 new_value = "TRUE"
 
 print(robot.read(variable_name))
