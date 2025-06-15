4d671ff7f5cf30e627ca48afc1c790f0dcf0b1e6
loic <loic.tournier@gmail.com>
2025-06-15 21:42:37 +0200

    trying to make a nice buffer mecanism

--- kukavar_client/make circle.py
+++ kukavar_client/make circle.py
@@ -64,43 +64,57 @@ def generate_circle_trajectory(radius, num_points):
         trajectory.append(trajstring)
     return trajectory
 
+def send_points(robot,trajectory,istart,iend, offset):
+    print(f"sendind points to the robot from trajectory {istart+offset} to {iend+offset}")
+    #send trajectory points to the robot
+    for i in range(istart+offset,iend+offset):
+        variable_name = f"COM_E6POS[{i+1-offset}]" # KRL arrays are typically 1-indexed
+        set_variable(robot, variable_name, trajectory[i])
+    robot.write("COM_LOOP_START", istart) # Set the number of trajectory points
+    robot.write("COM_LOOP_END", iend) # Set the number of trajectory points
+    print(f"{iend-istart} trajectory points sent to the robot.")
+
 if __name__ == "__main__":
     radius = 200  # Radius of the circle
-    num_points = 100 # Number of points in the trajectory
-    trajectory = ["{E6POS: X 10.41123, Y 1800.0, Z 2000.0, A -93.38996, B 89.96180, C 179.2077, S 2, T 35, E1 0.0, E2 0.0, E3 0.0, E4 0.0, E5 0.0, E6 0.0}",
-                  "{E6POS: X 10.41123, Y 1600.0, Z 2000.0, A -93.38996, B 89.96180, C 179.2077, S 2, T 35, E1 0.0, E2 0.0, E3 0.0, E4 0.0, E5 0.0, E6 0.0}",
-                  "{E6POS: X 10.41123, Y 1600.0, Z 1800.0, A -93.38996, B 89.96180, C 179.2077, S 2, T 35, E1 0.0, E2 0.0, E3 0.0, E4 0.0, E5 0.0, E6 0.0}"]
+    num_points = 200 # Number of points in the trajectory
+    trajectory = ["{E6POS: X 133.8644, Y 1899.212, Z 999.9999, A 5.259741E-04, B -4.623500E-05, C -89.99998, S 2, T 35, E1 0.0, E2 0.0, E3 0.0, E4 0.0, E5 0.0, E6 0.0}",
+                  "{E6POS: X 100.8644, Y 1600.212, Z 999.9999, A 5.259741E-04, B -4.623500E-05, C -89.99998, S 2, T 35, E1 0.0, E2 0.0, E3 0.0, E4 0.0, E5 0.0, E6 0.0}",
+                  "{E6POS: X 300.8644, Y 1899.212, Z 999.9999, A 5.259741E-04, B -4.623500E-05, C -89.99998, S 2, T 35, E1 0.0, E2 0.0, E3 0.0, E4 0.0, E5 0.0, E6 0.0}",
+                  "{E6POS: X 133.8644, Y 1899.212, Z 999.9999, A 5.259741E-04, B -4.623500E-05, C -89.99998, S 2, T 35, E1 0.0, E2 0.0, E3 0.0, E4 0.0, E5 0.0, E6 0.0}"]
     trajectory = generate_circle_trajectory(radius,num_points)
     robot = KukaVarProxyClient('192.168.1.5',7000)
     robot.connect()
     start_program(robot)
-    set_speed(robot, 10)  # Set speed to 10%
+    set_speed(robot, 100)  # Set speed to 10%
     read_position(robot)
 
-    #init
+    #----------init robot--------------------------
     robot.write("KVPMOVE_ENABLE", "FALSE") 
     #def approximation for linear move
-    robot.write("KVP_APO",3.0)
+    robot.write("KVP_APO",2.0)
     robot.write("NB_ADVANCE_POINTS",5)
 
-    print("sendind points to the robot")
-    #send trajectory points to the robot
-    for i, point in enumerate(trajectory):
-        variable_name = f"COM_E6POS[{i+1}]" # KRL arrays are typically 1-indexed
-        #print(point)
-        set_variable(robot, variable_name, point)
+    #send first 100 points to robot
+    send_points(robot,trajectory,1,100,0)
 
-    robot.write("COM_NEW_POINTS", str(len(trajectory))) # Set the number of trajectory points
-    print(f"All {len(trajectory)} trajectory points sent to the robot.")
-    
     # Enable trajectory mode
     robot.write("KVP_LIN_MOTION", "FALSE")
     robot.write("KVP_PTP_MOTION", "FALSE")   
     robot.write("KVP_TRAJECTORY_MODE", "TRUE") 
     robot.write("KVPMOVE_ENABLE", "TRUE")  
 
-   
     print("Program started")
+
+    #watch current point
+    current_index = 1
+    while current_index < 50 :
+        current_index = int(robot.read("COM_CURRENT_POINT_INDEX"))
+
+    #half of it reached sending other points + offset 100 ( 101 à 150)
+    send_points(robot,trajectory,1,50,100)
+
+
+    
 
     #read current trajectory state
     current_state = robot.read("COM_TRAJECTORY_FINISHED")
